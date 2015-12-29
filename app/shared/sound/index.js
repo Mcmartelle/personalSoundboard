66ef9129e79f1514a37d6ef58adf0e9c549543f2
Daniel Romero <daniel.romero@aofl.com>
2015-12-29 12:34:17 -0800

    Fixed buttons to have previous css styling before they were changed to directive

--- app/shared/sound/index.js
+++ app/shared/sound/index.js
@@ -1,35 +1,41 @@
 define(['app'], function(app) {
-	app.directive('sound', function() {
-		return {
-			restrict: 'E',
-			templateUrl: function(elem, attr){
-				var template = attr.type;
+    app.directive('sound', function() {
+        return {
+            restrict: 'E',
+            controller: 'soundController',
+            templateUrl: function(elem, attr) {
+                var template = attr.type;
 
-				if(template){
-					return 'app/shared/sound/index-' + template + '.html';
-				} else {
-					return 'app/shared/sound/index.html';
-				}
+                if (template) {
+                    return 'app/shared/sound/index-' + template + '.html';
+                } else {
+                    return 'app/shared/sound/index.html';
+                }
 
-			},
-			link: function(scope, element, attr) {
+            },
+            link: function(scope, element, attr) {
 
-				scope.play = function(text){
-						responsiveVoice.speak(text.speaktext,text.accent);
-				};
+                scope.play = function(text) {
+                    responsiveVoice.speak(text.speaktext, text.accent);
+                };
 
-				scope.pause = function(){
-					responsiveVoice.pause();
-				};
+                scope.pause = function() {
+                    responsiveVoice.pause();
+                };
 
-				scope.resume = function(){
-					responsiveVoice.resume();
-				};
+                scope.resume = function() {
+                    responsiveVoice.resume();
+                };
 
-				scope.stop = function(){
-					responsiveVoice.cancel();
-				};
-			}
-		};
-	});
+                scope.stop = function() {
+                    responsiveVoice.cancel();
+                };
+            }
+        };
+    });
+    app.controller('soundController', function($scope, $css) {
+        $css.bind({
+            href: 'app/shared/sound/index.css'
+        }, $scope);
+    });
 });
