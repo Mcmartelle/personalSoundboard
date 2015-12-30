5aa818b80792fb44b2377d34bf138d446034eaef
Matthew Martelle <mcmartelle@gmail.com>
2015-12-29 17:45:23 -0800

    added links to footer

--- app/components/core/footer/index.js
+++ app/components/core/footer/index.js
@@ -1,8 +1,26 @@
-define(['app'], function(app) {
+require.config({
+    paths: {
+        'navigation': 'app/shared/navigation/index',
+        'draggable': 'app/shared/draggable'
+    }
+});
+define([
+    'app',
+    'navigation',
+    'draggable'
+], function(app) {
     app.directive('footer', function() {
         return {
             restrict: 'E',
+            replace: true,
             templateUrl: 'app/components/core/footer/index.html',
+            controller: 'footerController',
+            controllerAs: 'footerCtrl'
         };
+    });
+    app.controller('footerController', function($scope, $css) {
+        $css.bind({
+            href: 'app/components/core/footer/index.css'
+        }, $scope);
     });
 });
