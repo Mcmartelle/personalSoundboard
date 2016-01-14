d8fc07235a45b8e9673f0875e57d6fe7fa6bb984
Matthew Martelle <mcmartelle@gmail.com>
2016-01-14 14:21:06 -0800

    modified for stand alone frontend operation

--- app/app.js
+++ app/app.js
@@ -1,43 +1,43 @@
 require.config({
-    paths: {}
+  paths: {}
 });
 define([
-    'angularAMD',
-    'angular-route',
-    'angular-animate',
-    'angular-aria',
-    'angular-material',
-    'angular-css',
+  'angularAMD',
+  'angular-route',
+  'angular-animate',
+  'angular-aria',
+  'angular-material',
+  'angular-css',
 ], function(angularAMD) {
-    var app = angular.module("SoundboardApp", ['ngRoute', 'ngMaterial', 'door3.css']);
+  var app = angular.module("SoundboardApp", ['ngRoute', 'ngMaterial', 'door3.css']);
 
-    app.config(function($routeProvider) {
-        var compLoc = 'app/components/';
-        var coreLoc = 'app/components/core/';
-        var shareLoc = 'app/shared/';
-        $routeProvider
-            .when("/home", angularAMD.route({
-                templateUrl: compLoc + 'home/index.html',
-                controllerUrl: compLoc + 'home/index.js',
-                controller: 'HomeController',
-                controllerAs: 'HomeCtrl'
-            }))
-            .when("/soundboard", angularAMD.route({
-                templateUrl: compLoc + 'soundboard/index.html',
-                controllerUrl: compLoc + 'soundboard/index.js',
-                controller: 'SoundboardController',
-                controllerAs: 'soundboardCtrl'
-            }))
-            .when("/create", angularAMD.route({
-                templateUrl: compLoc + 'create/index.html',
-                controllerUrl: compLoc + 'create/index.js',
-                controller: 'CreateController',
-                controllerAs: 'createCtrl'
-            }))
-            .otherwise({
-                redirectTo: '/home'
-            });
-    });
+  app.config(function($routeProvider) {
+    var compLoc = 'app/components/';
+    var coreLoc = 'app/components/core/';
+    var shareLoc = 'app/shared/';
+    $routeProvider
+      .when("/home", angularAMD.route({
+        templateUrl: compLoc + 'home/index.html',
+        controllerUrl: compLoc + 'home/index.js',
+        controller: 'HomeController',
+        controllerAs: 'HomeCtrl'
+      }))
+      .when("/soundboard", angularAMD.route({
+        templateUrl: compLoc + 'soundboard/index.html',
+        controllerUrl: compLoc + 'soundboard/index.js',
+        controller: 'SoundboardController',
+        controllerAs: 'soundboardCtrl'
+      }))
+      .when("/create", angularAMD.route({
+        templateUrl: compLoc + 'create/index.html',
+        controllerUrl: compLoc + 'create/index.js',
+        controller: 'CreateController',
+        controllerAs: 'createCtrl'
+      }))
+      .otherwise({
+        redirectTo: '/soundboard'
+      });
+  });
 
-    return angularAMD.bootstrap(app);
+  return angularAMD.bootstrap(app);
 });
